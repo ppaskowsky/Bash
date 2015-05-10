9bc34212621c1b876517dd0113ac96beec49ff2b
Peter Paskowsky <peter.paskowsky@gmail.com>
2015-05-10 18:24:20 -0400

    Fixed Typo

--- rasperry-pi-seedbox-setup-script.sh
+++ rasperry-pi-seedbox-setup-script.sh
@@ -11,7 +11,7 @@ mkdir /root/downloads
 mkdir /root/watch
 mkdir /root/session
 wget https://gist.githubusercontent.com/ppaskowsky/8e5f472d5e511f30fa46/raw/40bfa3632d6d2667ca2241ad1ea3ff0ca6eae052/.rtorrent.rc -O /root/.rtorrent.rc
-wget https://raw.githubusercontent.com/ppaskowsky/Bash/master/auto-linux-downloader-minimal.sh > /etc/cron.daily/download_script
+wget https://raw.githubusercontent.com/ppaskowsky/Bash/master/auto-linux-downloader-minimal.sh -O /etc/cron.daily/download_script
 chmod +x /etc/cron.daily/download_script
 /etc/cron.daily/download_script
 sed -i -e '$i \su -c "screen -d -m rtorrent"\n' /etc/rc.local
